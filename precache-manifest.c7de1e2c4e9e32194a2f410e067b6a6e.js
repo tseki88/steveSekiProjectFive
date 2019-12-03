ddf1cad169490a43826f00cc28ad7f8c50dc146c
tseki88 <teruhisaseki@gmail.com>
2019-12-02 19:53:39 -0500

    Updates

--- precache-manifest.c7de1e2c4e9e32194a2f410e067b6a6e.js
+++ precache-manifest.c7de1e2c4e9e32194a2f410e067b6a6e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7484f93760e06df72d0b6cbdada5b876",
+    "revision": "70fb5cf82543a17a8e8b4d301d69c6b0",
     "url": "/steveSekiProjectFive/index.html"
   },
   {
-    "revision": "34f2219f44441dcd111e",
+    "revision": "8d160fdc12f83114dcc7",
     "url": "/steveSekiProjectFive/static/css/main.5019f7b6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/steveSekiProjectFive/static/js/2.8053c89b.chunk.js"
   },
   {
-    "revision": "34f2219f44441dcd111e",
-    "url": "/steveSekiProjectFive/static/js/main.0a3c0096.chunk.js"
+    "revision": "8d160fdc12f83114dcc7",
+    "url": "/steveSekiProjectFive/static/js/main.75d81146.chunk.js"
   },
   {
     "revision": "638e09396e2562414d07",
