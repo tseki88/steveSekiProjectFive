863b96a52009c4fdc5667abfca9c57a6741d5411
tseki88 <teruhisaseki@gmail.com>
2019-12-02 19:24:33 -0500

    Updates

---
+++ precache-manifest.118ee7c2771197a8e4db0a885a4f4e43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c617188091b04bc5f9dcb8500adb3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7002d67709ddda7ebeab",
+    "url": "/static/css/main.5019f7b6.chunk.css"
+  },
+  {
+    "revision": "4097eeaa7e1796c15b1e",
+    "url": "/static/js/2.92d4c65b.chunk.js"
+  },
+  {
+    "revision": "7002d67709ddda7ebeab",
+    "url": "/static/js/main.43b7b9fc.chunk.js"
+  },
+  {
+    "revision": "6bbe9a06d439272266f1",
+    "url": "/static/js/runtime-main.8ae319f5.js"
+  }
+]);
